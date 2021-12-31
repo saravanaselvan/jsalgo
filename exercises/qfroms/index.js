73f8d28031abfe8294648d83fcf2526027f62265
saravanaselvan <spsaravanaselvan@gmail.com>
2021-12-31 14:19:20 +0530

    1. stack 2. qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,43 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(item) {
+    this.stack1.push(item);
+  }
+
+  remove() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+    let record = this.stack2.pop();
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    let record = this.stack2.peek();
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+    return record;
+  }
+}
 
 module.exports = Queue;
